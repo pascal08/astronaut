409d39735bf60497a19a1e0799669de543b91f27
Pascal Lubbers <pascallubbers8@hotmail.com>
2021-04-03 13:56:48 +0200

    WIP

--- src/Core/question.js
+++
@@ -1,9 +0,0 @@
-var Question = (function () {
-    function Question(question, answer) {
-        this.question = question;
-        this.answer = answer;
-    }
-    return Question;
-}());
-export { Question };
-//# sourceMappingURL=src/question.js.map
